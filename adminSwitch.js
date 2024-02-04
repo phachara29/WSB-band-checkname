8077cdb1fab200f1ba7b02e3bc7f8d06cbc5efcd
phachara29 <157819572+phachara29@users.noreply.github.com>
2024-02-04 13:13:59 +0700

    Add files via upload

---
+++ adminSwitch.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     // Get the admin switch button element
+//     var adminSwitchButton = document.getElementById('adminSwitchButton');
+
+//     // Add a click event listener to the button
+//     adminSwitchButton.addEventListener('click', function() {
+//         // Redirect to login.html when the button is clicked
+//         window.location.href = 'login.html';
+//     });
+// });
